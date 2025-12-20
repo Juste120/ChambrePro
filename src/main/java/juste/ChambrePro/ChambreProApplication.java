90d1bd227b79ae44fbdbe17bff8adfc99f47f79b
PAKOU Komi Juste <pakoujuste2019@gmail.com>
2025-12-20 01:59:25 +0000

    congig spring project

---
+++ src/main/java/juste/ChambrePro/ChambreProApplication.java
@@ -0,0 +1,13 @@
+package juste.ChambrePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChambreProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChambreProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/juste/ChambrePro/ChambreProApplicationTests.java
@@ -0,0 +1,13 @@
+package juste.ChambrePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChambreProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
